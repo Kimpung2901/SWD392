e2b6b4a513ab2cefd6ef96adab629884824a580c
Kimpung2901 <phungkimthi29012004@gmail.com>
2025-09-15 23:53:39 +0700

    library

--- WebNameProjectOfSWD/obj/Debug/net8.0/WebNameProjectOfSWD.AssemblyInfo.cs
+++ WebNameProjectOfSWD/obj/Debug/net8.0/WebNameProjectOfSWD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebNameProjectOfSWD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba92f114b46d656d466b48ae0dcba6946fd576aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebNameProjectOfSWD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebNameProjectOfSWD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
