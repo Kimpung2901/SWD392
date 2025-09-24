0b9997d1eca7077611371f51b96f3ccda7cb077e
Kimpung2901 <phungkimthi29012004@gmail.com>
2025-09-25 01:49:49 +0700

    fix conflict

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc40f6d0f44aec4a3f6bd522ceeb8a8f50a2b3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a12ee74b12687036a0b53a8efbd369a85b6d2703")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
